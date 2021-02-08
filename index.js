65fef6f1967ccbfb491a27ae262cb655c78c6ad1
shinyelee <shinye0213@naver.com>
2021-02-08 13:47:33 +0900

    api/users/logout -> /api/users/logout 오타 수정

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.get('/api/users/auth', auth, (req, res) => {
   })
 })
 
-app.get('api/users/logout', auth, (req, res) => {
+app.get('/api/users/logout', auth, (req, res) => {
 
   User.findOneAndUpdate({ _id: req.user._id },
     { token: "" }
