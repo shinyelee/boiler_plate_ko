b4e776e4d4e87a1889ab9a8f6b0a4dba8060596b
shinyelee <shinye0213@naver.com>
2021-02-13 01:47:22 +0900

    로그인 페이지 (2)

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,11 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios';
 
 function LandingPage() {
 
     useEffect(() => {
         axios.get('/api/hello')
-        .then(response => console.log(response.data))
+            .then(response => console.log(response.data))
     }, [])
 
     return (
